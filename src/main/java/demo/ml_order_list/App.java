34e572a53a306b9d3113915c62c156a2a23b138d
Jeferson Junior <jefersonguerrajr@live.com>
2021-12-19 14:18:17 -0300

    Adicionando Selenium ao projeto e criando verificação de usuário logado.

--- src/main/java/demo/ml_order_list/App.java
+++ src/main/java/demo/ml_order_list/App.java
@@ -1,13 +1,34 @@
 package demo.ml_order_list;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
- * Hello world!
- *
+ * @author Jeferson Junior
+ * 
  */
 public class App 
 {
-    public static void main( String[] args )
+	public static boolean usuarioLogado = false;
+	
+    public static void main( String[] args ) throws InterruptedException
     {
-        System.out.println( "Hello World!" );
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		System.out.println("Acessando pagina de login");
+		driver.get("https://test.com/sign-in");
+		
+		while(usuarioLogado == false) {
+			if(driver.getCurrentUrl().equals("https://test.com/home")) {
+				usuarioLogado = true;
+				break;
+			}
+			Thread.sleep(2000);
+			System.out.println("Aguardando login");
+		}
+		
+		System.out.println("Usuário logado!");
+		
+
     }
 }
